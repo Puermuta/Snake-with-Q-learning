c18c25b52e7afc9fdd741d27523d7ceded67eebe
Puermuta <19885813+Puermuta@users.noreply.github.com>
2026-01-15 21:54:43 +0100

    change restart to reset and fix fruit count logic

--- src/main.js
+++ src/main.js
@@ -2,18 +2,6 @@ import Grid from './components/grid.js';
 import Snake from './components/snake.js';
 import delay from './components/delay.js';
 
-let snake_container = document.getElementById("snake_game");
-let grid = new Grid(10, 10, snake_container);
-
-let snake = new Snake(grid);
-
-let directions = {
-    1: { r: 1, c: 0 },
-    2: { r: 0, c: 1 },
-    3: { r: -1, c: 0 },
-    4: { r: 0, c: -1},
-}
-
 // Functions
 function createSlider(sliderId, labelId, percentage = false) {
     const slider = document.getElementById(sliderId);
@@ -60,9 +48,9 @@ function startGame() {
 function stopGame() { 
     active = false;
 }
-function restartGame() {
+function resetGame() {
     snake.reset()
-    active = true;
+    active = false;
 }
  
 function setPosition(position) {
@@ -77,35 +65,48 @@ function setPosition(position) {
 const start = document.getElementById("start");
 //const pause = document.getElementById("pause");
 const stop = document.getElementById("stop");
-const restart = document.getElementById("restart");
+const reset = document.getElementById("reset");
 
 start.addEventListener("click", startGame);
 //pause.addEventListener("click", pauseGame);
 stop.addEventListener("click", stopGame);
-restart.addEventListener("click", restartGame);
-
+reset.addEventListener("click", resetGame);
 
 /*
 posStandard.addEventListener("click", setPosition("standard"));
 posRandom.addEventListener("click", setPosition("random"));
 posCustom.addEventListener("click", setPosition("custom"));
 */
-const fruitCount = document.getElementById("fruit-count");
-const fruitCountLabel = document.getElementById("fruit-count-label")
-fruitCount.addEventListener("input", () => {
-    fruitCountLabel.textContent = fruitCount.value;
-    snake.fruitCount = fruitCount.value;
-});
 
 // Selections
 createSelection('starting-position', '.pos-selection', 0);
 createSelection('border-mode', '.bm-selection', 1);
 
 // Sliders
+createSlider('fruit-count', 'fruit-count-label');
 createSlider('sim-speed', 'sim-speed-label');
 createSlider('alpha', 'alpha-label', true);
 createSlider('gamma', 'gamma-label', true);
 createSlider('epsilon', 'epsilon-label', true);
+
+// Game logic
+const fruitCount = document.getElementById('fruit-count');
+
+let snake_container = document.getElementById("snake_game");
+let grid = new Grid(10, 10, snake_container);
+let snake = new Snake(grid, fruitCount.value);
+
+fruitCount.addEventListener("input", () => {
+    snake.fruitCount = fruitCount.value;
+});
+
+
+let directions = {
+    1: { r: 1, c: 0 },
+    2: { r: 0, c: 1 },
+    3: { r: -1, c: 0 },
+    4: { r: 0, c: -1},
+}
 
 // Main loop
 let simSpeed = document.getElementById('sim-speed');
